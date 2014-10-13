78246cd2ea2a2e6e85a562e9eea4b31a75ec5d1d
Hemanth <heamnth7787@gmail.com>
2014-10-13 16:57:47 +0530

    Local enviornment config change to identify codestager host

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('endurance'),
+	'local' => array('endurance', 'ip-172-31-36-229'),
 
 ));
 
