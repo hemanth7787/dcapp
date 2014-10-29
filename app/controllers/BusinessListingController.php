6ad15f5eb9ad9ca7f524d9adc12136a85f926416
Hemanth <heamnth7787@gmail.com>
2014-10-29 17:53:42 +0530

    Dummy user connection based filtering of meber list

--- app/controllers/BusinessListingController.php
+++ app/controllers/BusinessListingController.php
@@ -24,17 +24,23 @@ class BusinessListingController extends \BaseController {
 
 
 		$p_list = array();
+
+		$count = 0;
 		foreach ($companyProfiles as $profile)
 	    {
 	    	// TODO #################################
 	    	// Check if users has connection ??
+//dummy
+if($count % 2 == 0){
+	$connection = 1;
 
-
-	    	$user_details = array('id'=>$profile->user->id,
+		    	$user_details = array('id'=>$profile->user->id,
 	    		'name'=>$profile->user->name,
 	    		'email'=>$profile->user->email,
 	    		'mobile'=>$profile->user->mobile,
-	    		'chamber_profile'=>$profile->user->chamber_profile);
+	    		'chamber_profile'=>$profile->user->chamber_profile,
+	    		'connected'=>$connection
+	    		);
 
 	    	$profile_details = array('id'=>$profile->id,
 	    	    'company_name'=>$profile->company_name,
@@ -44,11 +50,115 @@ class BusinessListingController extends \BaseController {
 	    		'trade_license_number'=>$profile->trade_license_number,
 	    		'verified'=>$profile->verified,
 	    		'image'=>$profile->image);
+}
+else{
+$connection = 0;
+
+	    	$user_details = array('id'=>$profile->user->id,
+	    		'name'=>$profile->user->name,
+	    		//'email'=>$profile->user->email,
+	    		//'mobile'=>$profile->user->mobile,
+	    		'chamber_profile'=>$profile->user->chamber_profile,
+	    		'connected'=>$connection
+	    		);
+
+	    	$profile_details = array('id'=>$profile->id,
+	    	    'company_name'=>$profile->company_name,
+	    		'designation'=>$profile->designation,
+	    		//'company_email'=>$profile->company_email,
+	    		//'membership_number'=>$profile->membership_number,
+	    		'trade_license_number'=>$profile->trade_license_number,
+	    		'verified'=>$profile->verified,
+	    		'image'=>$profile->image);
+}
+$count++;
+
+
 
 	    	array_push($p_list, array('company_profile'=>$profile_details,'user'=>$user_details));
 	        //$p_list[] = array(...);
 	    }
 	    return Response::json($p_list);
+
+	}
+
+	public function dummy(){
+		$client = new SoapClient('https://dcsoamw.dubaichamber.com:8012/DCCICommercialDirectory/ProxyServices/DCCICommercialDirectoryProxy');
+		$xml = '<?xml version="1.0" encoding="UTF-8"?>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:dcq="http://siebel.com/DCQueryMobCommDir" xmlns:dc="http://www.siebel.com/xml/DC%20Query%20Member%20Info">
+<soapenv:Header>
+<wsse:Security xmlns:wsse="http://schemas.xmlsoap.org/ws/2002/07/secext">
+<wsse:UsernameToken xmlns:wsu="http://schemas.xmlsoap.org/ws/2002/07/utility">
+<wsse:Username>MOBILEUSER</wsse:Username>
+<wsse:Password Type="wsse:PasswordText">MOBILEUSER</wsse:Password>
+</wsse:UsernameToken>
+</wsse:Security>
+</soapenv:Header>
+   <soapenv:Body>
+      <dcq:QBECommDir>
+         <SiebelMessage>
+            <dc:ListOfDcQueryMemberInfo>
+               <!--Zero or more repetitions:-->
+               <dc:Account>
+                  <!--Optional:-->
+                  <dc:CSN/>
+                  <dc:DCNameArabic/>
+                  <dc:DCNameEnglish>* Futtaim *</dc:DCNameEnglish>
+                  <!--Optional:-->
+                  <dc:MainEmailAddress/>
+                  <!--Optional:-->
+                  <dc:MainFaxNumber/>
+                  <!--Optional:-->
+                  <dc:MainPhoneNumber/>
+                  <!--Optional:-->
+                  <dc:Name/>
+                  <!--Optional:-->
+                  <dc:ListOfAccount_BusinessAddress>
+                     <!--Zero or more repetitions:-->
+                     <dc:Account_BusinessAddress>
+                        <!--Optional:-->
+                        <dc:ApartmentNumber/>
+                        <dc:City/>
+                        <!--Optional:-->
+                        <dc:Country/>
+                        <!--Optional:-->
+                        <dc:DCAreaPOBox/>
+                        <!--Optional:-->
+                        <dc:DCBuildingNameArabic/>
+                        <!--Optional:-->
+                        <dc:DCBuildingNammeEnglish/>
+                        <!--Optional:-->
+                        <dc:Province/>
+                        <dc:Street/>
+                        <dc:StreetAddress/>
+                        <dc:AddressName/>
+                     </dc:Account_BusinessAddress>
+                  </dc:ListOfAccount_BusinessAddress>
+                  <!--Optional:-->
+                  <dc:ListOfAccount_DCAccountActivity>
+                     <!--Zero or more repetitions:-->
+                     <dc:Account_DCAccountActivity>
+                        <!--Optional:-->
+                        <dc:DCActivityCode/>
+                        <!--Optional:-->
+                        <dc:DCActivityDesArabic/>
+                        <!--Optional:-->
+                        <dc:DCActivityDescriptionEng/>
+                     </dc:Account_DCAccountActivity>
+                  </dc:ListOfAccount_DCAccountActivity>
+               </dc:Account>
+            </dc:ListOfDcQueryMemberInfo>
+         </SiebelMessage>
+      </dcq:QBECommDir>
+   </soapenv:Body>
+</soapenv:Envelope>';
+    			$params = array (
+    			"userName" => 'MOBILEUSER',
+    			"password" => 'MOBILEUSER',
+    			);
+$response = $client->__soapCall('authWithProfile', $params);
+				$parser = simplexml_load_string($response);
+				return var_dump($parser);
 
 	}
 
